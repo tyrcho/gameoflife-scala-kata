14467bbb0cd9f20b02bde17fa21f9adcd53be141
[a518291] Michel Daviot <michel.daviot@atos.net>
2016-07-28 16:21:50 +0200

    refactoring

--- src/main/scala/gameoflife/GameOfLife.scala
+++ src/main/scala/gameoflife/GameOfLife.scala
@@ -2,47 +2,59 @@ package gameoflife
 
 import java.io.InputStream
 
-case class GameOfLife(rows: Vector[String]) {
-  val width = rows.head.size
-  val height = rows.size
-
-  def neighbours(i: Int, max: Int): Seq[Int] = {
-    if (i == 0) Seq(max - 1, 0, 1)
-    else if (i == max - 1) Seq(max - 2, max - 1, 0)
-    else i - 1 to i + 1
-
-  }
+case class GameOfLife(width: Int, height: Int, data: Set[(Int, Int)]) {
 
   def liveNeighbours(row: Int, col: Int): Int = {
     for {
-      r <- neighbours(row, height) // 0 until height
-      line = rows(r)
-      w <- neighbours(col, width) //0 until width
+      r <- neighbours(row, height)
+      w <- neighbours(col, width)
       if r != row || w != col
-    } yield if (line(w) == 'X') 1 else 0
+    } yield if (isLive(w, r)) 1 else 0
   }.sum
 
-  def nextStep: GameOfLife = GameOfLife {
+  def nextStep: GameOfLife = copy(data = {
     for {
-      i <- (0 until height).toVector
-    } yield {
-      for {
-        j <- 0 until width
-      } yield {
-        if ((liveNeighbours(i, j) == 2 && rows(i)(j) == 'X') || liveNeighbours(i, j) == 3) 'X'
-        else '.'
-      }
+      i <- 0 until height
+      j <- 0 until width
+      ln = liveNeighbours(i, j)
+      if (ln == 2 && isLive(j, i)) || ln == 3
+    } yield (j, i)
+  }.toSet)
 
-    }.mkString
-  }
+  def rows: Vector[String] = for {
+    i <- (0 until height).toVector
+  } yield {
+    for {
+      j <- 0 until width
+    } yield if (isLive(j, i)) 'X' else '.'
+  }.mkString
 
   def print() = {
     rows.foreach(println)
     println()
   }
+
+  private def isLive(x: Int, y: Int) = data.contains((x, y))
+
+  private def neighbours(i: Int, max: Int): Seq[Int] = {
+    if (i == 0) Seq(max - 1, 0, 1)
+    else if (i == max - 1) Seq(max - 2, max - 1, 0)
+    else i - 1 to i + 1
+  }
 }
 
 object GameOfLife {
+  def apply(rows: Vector[String]): GameOfLife = {
+    val width = rows.head.size
+    val height = rows.size
+    val data = for {
+      x <- 0 until width
+      y <- 0 until height
+      if rows(y)(x) == 'X'
+    } yield (x, y)
+    GameOfLife(width, height, data.toSet)
+  }
+
   def load(resource: String): GameOfLife = {
     val stream = GameOfLife.getClass.getResourceAsStream(resource)
     assert(stream != null, "resource not found")
--- src/main/scala/gameoflife/Main.scala
+++ src/main/scala/gameoflife/Main.scala
@@ -1,10 +1,13 @@
 package gameoflife
 
 object Main extends App {
-  var initial = GameOfLife.load("/acorn.txt")
+  var initial = GameOfLife.load("/acorn.txt").copy(width = 100, height = 50)
+  var round = 1
   while (true) {
     initial.print()
     initial = initial.nextStep
-    readLine()
+    println(round)
+    round += 1
+    Thread.sleep(100)
   }
 }
